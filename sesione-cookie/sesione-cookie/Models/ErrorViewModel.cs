f608cd030fd9164665c8e6286f600528f72c917e
Emanuel Nguema Oyono <enguema@mait.gq>
2026-07-03 17:35:50 +0100

    second commit

---
+++ sesione-cookie/sesione-cookie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sesione_cookie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
